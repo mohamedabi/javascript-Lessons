4a446644cf5189b951cb55e12e38ee36edbf75c5
Mohamed Abi <abi.moha7@gmail.com>
2022-06-03 22:46:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.write("<h1> abilowohnung</h1>");
+
+
+var user="helloscasc";
+
+console.log(user);
+
+
+console.log('user');
+
+
+let a = "we Love";
+let b = "Javascript";
+
+document.write ( a + " " + b);
+
+console.log(a , b);
+
+
