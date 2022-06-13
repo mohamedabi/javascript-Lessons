42fd17af16b7d3177bc7ab2ad2d97986233e7db5
Mohamed Abi <abi.moha7@gmail.com>
2022-06-13 22:03:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let d ="-100";
+let e ="20";
+let f ="30";
+let g = true;
+
+// 2000
+//173
+
+
+
+
+
+console.log(d * -e); // 2000
+
+console.log(++g * ++e + +f - --d);  // 173
+
+
+console.log(Math.round(25.7));
+console.log(Math.round(25.2));
+
+console.log(Math.ceil(23.2)); //   IMMER WIRD EINE PLUS +1
+
+console.log(Math.floor(23.7)); // IMMER WIR EINE MINUS -1
+
+console.log(Math.min(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)); //   WIRD DIE KLEINESTE ZAHL GENOMMEN 10
+console.log(Math.max(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)); //  WIRD DIE GRÜßESTE ZAHL GENOMMEN 100
+
+console.log(Math.pow(2, 4)); // 2 * 2 * 2 * 2 = 16
+
+console.log(Math.random());
+
+
